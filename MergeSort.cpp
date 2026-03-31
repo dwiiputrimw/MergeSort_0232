d00cd681d12fa6923313e76fbf9096ab92307fd8
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-03-31 11:23:35 +0700

    Menambahkan input elemen array

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -20,6 +20,16 @@ void input()
             cout << "\nMaksimum panjang array yang dapat dimasukkan adalah 20." << endl;
         }
     }
+    cout << endl;
+    cout << "================================================" << endl;
+    cout << "=============Masukkan Element Array=============" << endl;
+    cout << "================================================" << endl;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << i + 1 << ": ";
+        cin >> arr[i];
+    }
 }
     
 
